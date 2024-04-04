3d8672104c1fafb243dac80a7a017ec1021cb9c7
JAndersM <jandersmartinsson@gmail.com>
2024-04-04 17:05:48 +0200

    Create addon_add_line.py

---
+++ addon_add_line.py
@@ -0,0 +1,90 @@
+bl_info = {
+    "name": "New Line",
+    "author": "jam007",
+    "version": (1, 0),
+    "blender": (4, 0, 0),
+    "location": "View3D > Add > Mesh > New Object",
+    "description": "Adds a new Line Object",
+    "warning": "",
+    "doc_url": "",
+    "category": "Add Mesh",
+}
+
+
+import bpy
+from bpy.types import Operator
+from bpy.props import FloatVectorProperty
+from bpy_extras.object_utils import AddObjectHelper, object_data_add
+from mathutils import Vector
+
+
+def add_object(self, context):
+    scale_z = self.scale.z
+
+    verts = [
+        Vector((0, 0, -0.5*scale_z)),
+        Vector((0, 0, 0.5*scale_z)),
+    ]
+
+    edges = [[0,1]]
+    faces = []
+
+    mesh = bpy.data.meshes.new(name="Line")
+    mesh.from_pydata(verts, edges, faces)
+    # useful for development when the mesh may be invalid.
+    # mesh.validate(verbose=True)
+    object_data_add(context, mesh, operator=self)
+
+
+class OBJECT_OT_add_object(Operator, AddObjectHelper):
+    """Create a new Line Object"""
+    bl_idname = "mesh.add_object"
+    bl_label = "Add Line Object"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    scale: FloatVectorProperty(
+        name="scale",
+        default=(1.0, 1.0, 1.0),
+        subtype='TRANSLATION',
+        description="scaling",
+    )
+
+    def execute(self, context):
+
+        add_object(self, context)
+
+        return {'FINISHED'}
+
+
+# Registration
+
+def add_object_button(self, context):
+    self.layout.operator(
+        OBJECT_OT_add_object.bl_idname,
+        text="Add Line",
+        icon='IPO_LINEAR')
+
+
+# This allows you to right click on a button and link to documentation
+def add_object_manual_map():
+    url_manual_prefix = "https://docs.blender.org/manual/en/latest/"
+    url_manual_mapping = (
+        ("bpy.ops.mesh.add_object", "scene_layout/object/types.html"),
+    )
+    return url_manual_prefix, url_manual_mapping
+
+
+def register():
+    bpy.utils.register_class(OBJECT_OT_add_object)
+    bpy.utils.register_manual_map(add_object_manual_map)
+    bpy.types.VIEW3D_MT_mesh_add.append(add_object_button)
+
+
+def unregister():
+    bpy.utils.unregister_class(OBJECT_OT_add_object)
+    bpy.utils.unregister_manual_map(add_object_manual_map)
+    bpy.types.VIEW3D_MT_mesh_add.remove(add_object_button)
+
+
+if __name__ == "__main__":
+    register()
