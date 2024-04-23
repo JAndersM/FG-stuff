8d842a38aa4a758d2a359898cb5b9884ca619ded
JAndersM <jandersmartinsson@gmail.com>
2024-04-23 17:27:32 +0200

    All variations

--- getaxis.py
+++ getaxis.py
@@ -2,6 +2,7 @@
 #Print animation info for objects named *axis asumed to be a line object
 import bpy
 
+
 for col in bpy.data.collections:
    for obj in col.objects:
        if obj.name.endswith("axis"):
@@ -9,11 +10,17 @@ for col in bpy.data.collections:
            v0=obj.matrix_world @ obj.data.vertices[0].co
            v1=obj.matrix_world @ obj.data.vertices[1].co
            print(name)
-           print("  <center>")
-           print("    <x-m> {0:.4f} </x-m>".format((v0.x+v1.x)/2))
-           print("    <y-m> {0:.4f} </y-m>".format((v0.y+v1.y)/2))
-           print("    <z-m> {0:.4f} </z-m>".format((v0.z+v1.z)/2))
-           print("  </center>")
+#           print("  <center>")
+#           print("    <x-m> {0:.4f} </x-m>".format((v0.x+v1.x)/2))
+#           print("    <y-m> {0:.4f} </y-m>".format((v0.y+v1.y)/2))
+#           print("    <z-m> {0:.4f} </z-m>".format((v0.z+v1.z)/2))
+#           print("  </center>")
+#           v=v1 -v0
+#           print("  <axis>")
+#           print("    <x-m> {0:.4f} </x-m>".format(v.x))
+#           print("    <y-m> {0:.4f} </y-m>".format(v.y))
+#           print("    <z-m> {0:.4f} </z-m>".format(v.z))
+#           print("  </axis>")
            print("  <axis>")
            print("    <x1-m> {0:.4f} </x1-m>".format(v0.x))
            print("    <y1-m> {0:.4f} </y1-m>".format(v0.y))
